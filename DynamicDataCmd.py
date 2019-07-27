0b3a59ec5902b338c37726f8f0cafc7bf92ddd36
Mark Ganson TheMarkster <39143564+mwganson@users.noreply.github.com>
2019-07-27 15:38:21 -0500

    Version 1.60 (#21)
    * version 1.60, add support for ViewObject properties
    
    * version 1.60 add parameter to enable/disable
    
    ViewObject support

--- DynamicDataCmd.py
+++ DynamicDataCmd.py
@@ -26,9 +26,9 @@
 __title__   = "DynamicData"
 __author__  = "Mark Ganson <TheMarkster>"
 __url__     = "https://github.com/mwganson/DynamicData"
-__date__    = "2019.07.26"
-__version__ = "1.50"
-version = 1.50
+__date__    = "2019.07.27"
+__version__ = "1.60"
+version = 1.60
 mostRecentTypes=[]
 mostRecentTypesLength = 5 #will be updated from parameters
 
@@ -149,14 +149,20 @@ class DynamicDataSettingsCommandClass(object):
         pg = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/DynamicData")
         keep = pg.GetBool('KeepToolbar',True)
         mostRecentTypesLength = pg.GetInt('mruLength',5)
-        items=["Keep the toolbar active","Do not keep the toolbar active","Change length ("+str(mostRecentTypesLength)+") of most recently used type list"]
+        items=["Keep the toolbar active","Do not keep the toolbar active","Change length ("+str(mostRecentTypesLength)+") of most recently used type list","Support ViewObject properties", "Do not support ViewObject properties","Cancel"]
         item,ok = QtGui.QInputDialog.getItem(window,'DynamicData','Settings\n\nSelect the settings option\n',items,0,False)
-        if ok and item == items[0]:
+        if ok and item == items[-1]:
+            return
+        elif ok and item == items[0]:
             keep = True
             pg.SetBool('KeepToolbar', keep)
         elif ok and item==items[1]:
             keep = False
             pg.SetBool('KeepToolbar', keep)
+        elif ok and item==items[3]:
+            pg.SetBool('SupportViewObjectProperties',True)
+        elif ok and item==items[4]:
+            pg.SetBool('SupportViewObjectProperties',False)
         elif ok and item==items[2]:
             count,ok = QtGui.QInputDialog.getInt(window,'DynamicData','Settings\n\nHow many items in most recently used type list?\n\nCurrent setting = '+str(mostRecentTypesLength)+'\n',mostRecentTypesLength,0,20,1)
             if ok:
@@ -976,7 +982,7 @@ Enter the name for the new property\n',text=name)
                     name = 'dd'+cap(name)
 
                 try:
-                    toObj.addProperty('App::Property'+propertyType, name,'Copied from: '+fromObj.Label,'['+propertyType+']')
+                    toObj.addProperty('App::Property'+propertyType.replace('(ViewObject)',''), name,'Copied from: '+fromObj.Label,'['+propertyType+']')
                     toProperty = {'name':name,'type':propertyType,'value':property['value']}
                 except:
                     FreeCAD.Console.PrintError('DynamicData: Exception trying to add property ('+property['name']+')\n')
@@ -1027,7 +1033,9 @@ Enter the name for the new property\n',text=name)
                 return
             fromProperty=fromProperty[0] #returns a list, but only valid for copy modes, not set modes
             if not breakOnly:
-                toProperty = self.getProperty(toObj,'\n\nPrevious Selection: '+fromObj.Label+':'+fromProperty['name']+' ('+str(fromProperty['value'])+')\n\nChoose the TO property of '+toObj.Label+'\n',matchType=fromProperty['type'])
+                toProperty = self.getProperty(toObj,'\n\nPrevious Selection: '+fromObj.Label+':'+fromProperty['name']+' ('+str(fromProperty['value'])+')\n\nChoose the TO property of '+toObj.Label+'\n', matchType=fromProperty['type'].replace('(ViewObject)',''))
+                if not toProperty:
+                    return #user canceled
                 toProperty=toProperty[0]
             else:
                 toProperty = fromProperty
@@ -1035,7 +1043,10 @@ Enter the name for the new property\n',text=name)
                 return
             if not breakOnly:
                 try:
-                    setattr(toObj,toProperty['name'],fromProperty['value'])
+                    if '(ViewObject)' in toProperty['type']:
+                        setattr(toObj.ViewObject, toProperty['name'],fromProperty['value'])
+                    else:
+                        setattr(toObj,toProperty['name'],fromProperty['value'])
                 except:
                     FreeCAD.Console.PrintError(\
 'DynamicData: Exception trying to set property value ('+str(fromProperty['value'])+')\n\
@@ -1061,9 +1072,11 @@ To Object: '+toObj.Label+', To Property: '+toProperty['name']+', type: '+toPrope
         set operation is when existing property value was changed in toObj"""
 
         #User might not want to do this, but we check to see if we can do it before asking
-        if fromProperty['type'] in nonLinkableTypes:
+        if toProperty['type'] in nonLinkableTypes:
             return
-        #todo: ask user if he wants the parametric linking
+        #ViewObject does not have setExpression(), so do not try to parametrically link
+        if '(ViewObject)' in fromProperty['type'] or '(ViewObject)' in toProperty['type']:
+            return
         breakLink = False
         if not breakOnly:
             window = QtGui.QApplication.activeWindow()
@@ -1133,7 +1146,7 @@ To Object: '+toObj.Label+', To Property: '+toProperty['name']+', type: '+toPrope
                 toObj.setExpression(toProperty['name'], None)
         except:
             FreeCAD.Console.PrintError(\
-'DynamicData: Exception tryin to parametrically link ('+str(fromProperty['value'])+')\n\
+'DynamicData: Exception trying to parametrically link ('+str(fromProperty['value'])+')\n\
 Could be a property type mismatch\n\
 \n\
 From Object: '+fromObj.Label+', From Property: '+fromProperty['name']+', type: '+fromProperty['type']+'\n\
@@ -1145,6 +1158,8 @@ To Object: '+toObj.Label+', To Property: '+toProperty['name']+', type: '+toPrope
            property will be in the form of a list of dictionary objects
            with keys 'type', 'name', 'value' """
         available = []
+        pg = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/DynamicData")
+        supportViewObjectProperties = pg.GetBool('SupportViewObjectProperties', True)
         propertiesList = obj.PropertiesList
         whiteList=['Proxy','ExpressionEngine','DynamicData','Label','Shape']
         for prop in propertiesList:
@@ -1157,12 +1172,25 @@ To Object: '+toObj.Label+', To Property: '+toProperty['name']+', type: '+toPrope
             typeId = obj.getTypeIdOfProperty(prop)[13:] #strip "App::Property" from beginning
             if typeId in self.getPropertyTypes():
                 available.append({'type':typeId,'value':p,'name':prop})
+        if supportViewObjectProperties:
+            viewPropertiesList = obj.ViewObject.PropertiesList
+            for vprop in viewPropertiesList:
+                if vprop in whiteList:
+                    continue
+                vp = getattr(obj.ViewObject,vprop)
+                strType = str(type(vp))
+                types = self.getPropertyTypes()
+                typeFound = False;
+                typeId = obj.ViewObject.getTypeIdOfProperty(vprop)[13:] #strip "App::Property" from beginning
+                if typeId in self.getPropertyTypes():
+                    available.append({'type':'(ViewObject)'+typeId,'value':vp,'name':vprop})
+
         items=[]
         moved=[]
         for ii in range(0,len(available)):
             a = available[ii]
             items.append("name: "+a['name']+", type: "+a['type']+", value: "+str(a['value']))
-            if matchType==a['type']: #put same types at top of list
+            if matchType==a['type'].replace('(ViewObject)',''): #put same types at top of list
                 items.insert(0,items[-1])
                 items.pop(-1)
                 moved.append(ii)
