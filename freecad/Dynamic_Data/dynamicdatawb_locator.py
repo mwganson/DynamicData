c515a57fd317605fb39eaa463682e9448d69efde
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-11-24 06:24:47 -0500

    Add SPDX License Identifiers

--- freecad/Dynamic_Data/dynamicdatawb_locator.py
+++ freecad/Dynamic_Data/dynamicdatawb_locator.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
 ###############################################################################
 #
 #  Copyright 2018 Mark Ganson <TheMarkster> mwganson at gmail
